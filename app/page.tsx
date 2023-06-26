ea0a4285c939c9a7a89d56410dba3ff54cfd5d83
Alex Wulf <alexaethelwulf@gmail.com>
2023-06-25 23:41:40 -0500

    Building the Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-blue-400 text-7xl'>Open Table Clone - NextJS 13</h1>
-    </main>
+    <></>
   )
 }
