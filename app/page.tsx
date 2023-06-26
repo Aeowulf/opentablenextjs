e2262870d6dc42987593e1bd5ebbc77aabc763dd
Alex Wulf <alexaethelwulf@gmail.com>
2023-06-26 00:30:29 -0500

    Finished Card component prototype

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
 import Header from '@/components/Header'
+import Card from '@/components/Card'
 
 export default function Home() {
   return (
     <main>
       <Header />
+
+      {/* CARD AREA */}
+      <div className='py-3 px-36 mt-10 flex flex-wrap'>
+        <Card />
+      </div>
+      {/* CARD AREA */}
     </main>
   )
 }
