42c552a47402696826a38ba779add53a547abf2d
Alex Wulf <alexaethelwulf@gmail.com>
2023-06-27 20:58:16 -0500

    126. The Menu Page

--- app/restaurant/[id]/menu/page.tsx
+++ app/restaurant/[id]/menu/page.tsx
@@ -1,9 +1,22 @@
 import RestaurantNav from "@/components/RestaurantNav"
+import MenuCard from "@/components/MenuCard"
 
 export default function Home() {
   return (
     <div className='bg-white w-[100%] rounded p-3 shadow'>
       <RestaurantNav />
+
+      <main className="bg-white mt-5">
+        <div>
+          <div className="mt-4 pb-1 mb-1">
+            <h1 className="font-bold text-4xl">Menu</h1>
+          </div>
+
+          <div className="flex flex-wrap justify-between">
+            <MenuCard />
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
---
+++ components/MenuCard.tsx
@@ -0,0 +1,15 @@
+const MenuCard = () => {
+  return (
+    <div className='border rounded p-3 w-[49%] mb-3'>
+      <h3 className="font-bold text-lg">Surf and Turf</h3>
+
+      <p className='font-light mt-1 text-sm'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam unde ea id voluptates culpa? Fugit, possimus modi! Sequi ratione ipsam provident ipsum eligendi adipisci? In quasi amet eveniet veniam fuga!
+      </p>
+
+      <p className="mt-7">$80.00</p>
+    </div>
+  )
+}
+
+export default MenuCard
