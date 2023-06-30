70d59c129c86750fb47f8d806367dc881b13a491
Alex Wulf <alexaethelwulf@gmail.com>
2023-06-29 21:27:06 -0500

    Finished non-dynamic demo Search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -15,7 +15,9 @@ export default function Home() {
       </div>
     </div>
 
+    
     <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
+      {/* SEARCH SIDE BAR */}
       <div className='w-1/5'>
         <div className="border-b pb-4">
           <h1 className='mb-2'>Region</h1>
@@ -54,6 +56,43 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* SEARCH SIDE BAR */}
+      
+      {/* RESTAURANT SEARCH CARD */}
+      <div className='w-5/6'>
+        <div className="border-b flex pb-5">
+          <img 
+            src="https://resizer.otstatic.com/v2/photos/xlarge/2/52836347.webp" 
+            alt="" 
+            className='w-44 rounded' 
+          />
+
+          <div className="pl-5">
+            <h2 className="text-3xl">Aiana Restaurant Collective</h2>
+
+            <div className="flex items-start">
+              <div className="flex mb-2">
+                *****
+              </div>
+
+              <p className="ml-2 text-sm">Awesome</p>
+            </div>
+
+            <div className="mb-9">
+              <div className="font-light flex text-reg">
+                <p className="mr-4">$$$</p>
+                <p className="mr-4">Mexican</p>
+                <p className="mr-4">Ottawa</p>
+              </div>
+            </div>
+
+            <div className='text-red-600'>
+              <a href="/">View more information</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* RESTAURANT SEARCH CARD */}
     </div>
     </>
   )
