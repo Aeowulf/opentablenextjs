61121ff00d4049056fa1b5921c34a01f2bd2a4b6
Alex Wulf <alexaethelwulf@gmail.com>
2023-07-09 16:06:51 -0500

    Added Loading state

---
+++ app/loading.tsx
@@ -0,0 +1,22 @@
+import HomeHeader from "./_components/HomeHeader"
+
+const Loading = () => {
+  return (
+    <main>
+      <HomeHeader />
+
+      <div className="py-3 px-36 mt-10 flex flex-wrap">
+        {[1,2,3,4,5,6,7,8,9,10,11,12].map(num => (
+          <div 
+            key={num} 
+            className='animate-pulse bg-slate-200 w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer'
+          >
+
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default Loading
