2e299c0d6780bc4157518eb12c7aec624b0e1839
Alex Wulf <alexaethelwulf@gmail.com>
2023-08-13 17:51:21 -0500

    Three Week Vacay Over - Authentication State

--- app/_components/AuthModal.tsx
+++ app/_components/AuthModal.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import React, {useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import AuthModalInputs from './AuthModalInputs';
+import useAuth from '@/hooks/useAuth';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -22,6 +23,8 @@ export default function AuthModal({ signingIn } : {signingIn : boolean}) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const { signIn, signUp} = useAuth()
 
   const renderContent = (signInContent : string, signUpContent : string) => {
     return signingIn ? signInContent : signUpContent
@@ -42,6 +45,28 @@ export default function AuthModal({ signingIn } : {signingIn : boolean}) {
     city : '',
     password : '',
   })
+
+  const [disabled, setDisabled] = useState(true)
+
+  useEffect(() => {
+    if (signingIn) {
+      if (inputs.password && inputs.email) {
+        return setDisabled(false)
+      }
+    } else {
+      if (inputs.firstName && inputs.lastName && inputs.email && inputs.phone && inputs.city && inputs.password) {
+        return setDisabled(false)
+      }
+    }
+
+    setDisabled(true)
+  }, [inputs])
+
+  const handleClick = () => {
+    if (signingIn) {
+      signIn({ email: inputs.email, password: inputs.password })
+    }
+  }
 
   return (
     <div>
@@ -76,7 +101,11 @@ export default function AuthModal({ signingIn } : {signingIn : boolean}) {
                   signingIn={signingIn} 
                 />
 
-                <button className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400'>
+                <button 
+                  className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400' 
+                  disabled={disabled}
+                  onClick={handleClick}
+                >
                   {renderContent(
                     "Sign In", 
                     "Create Account",
